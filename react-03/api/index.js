c307927f0a78c8c656e1a0c884a62e54fb2fa30f
Gustavo Henrique <gustavo.00000854561@unicap.br>
2026-09-22 11:33:25 +0000

    mudança de chaves do back4app

--- react-03/api/index.js
+++ react-03/api/index.js
@@ -6,8 +6,8 @@ const instance = axios.create({
   baseURL: "https://parseapi.back4app.com",
   timeout: 5000,
   headers: {
-    "X-Parse-Application-Id": "t4wZSEfltZ4QtFBZX5LY9hyyx4I45uAbjsrwzCAi",
-    "X-Parse-REST-API-Key": "ZXx7xAlvFOyZith0HDnSpWdlHxnnATKsdgakYg4Q",
+    "X-Parse-Application-Id": "vd8FqpzM5llAAYPjQasyxGMjrtWR5aw4gTThtvar",
+    "X-Parse-REST-API-Key": "QVq5o20Pgnak48blizULHVn7tckkBRLz29zFN46e",
   },
 });
 
