0e85497fec4431ba60e53cc5e7b07dd6ef635c6e
Gustavo Henrique <gustavo.00000854561@unicap.br>
2026-09-22 11:40:17 +0000

    correção de texto

--- react-03/app/listaTarefas/page.js
+++ react-03/app/listaTarefas/page.js
@@ -75,7 +75,7 @@ export default function ListaTarefas() {
           checked={filtrarConcluidas}
           onChange={toggleFiltrarConcluidas}
         />
-        Só as não concluídas
+        Mostrar só as concluídas
         <hr />
         <ul>
           {tarefas.map((tarefa) => (
